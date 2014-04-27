c8cae57d2f42606b6f78a8ba83fa3e4b99792a6c
Joe Fitter <joefitter@hotmail.com>
2014-04-27 19:25:58 +0100

    added latest version of backbone-tooltip

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-requirejs.config({
+require.config({
   paths: {
     'backbone': 'vendor/backbone/backbone',
     'underscore': 'vendor/underscore/underscore',
@@ -6,6 +6,15 @@ requirejs.config({
     'stache': 'vendor/requirejs-mustache/stache',
     'text': 'vendor/requirejs-text/text',
     'mustache': 'vendor/mustache/mustache'
+  },
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    underscore: {
+      exports: '_'
+    }
   },
   stache: {
     extension: '.template',
